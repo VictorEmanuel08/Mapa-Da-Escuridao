cb35e5c2e20d4d275904987b3a78d48e57e94ae3
Ramon Maia <ramoon.maia23@gmail.com>
2024-07-09 17:10:42 -0300

    ajustado o delete e adicionando alert nas operações de del, post e put

--- src/components/Modals/EditMarker/index.jsx
+++ src/components/Modals/EditMarker/index.jsx
@@ -3,8 +3,9 @@ import { FaTrashAlt } from "react-icons/fa";
 import iconOn from "../../../assets/iconOn.svg";
 import iconOff from "../../../assets/iconOff.svg";
 import { app } from "../../../api/api";
+import Swal from "sweetalert2";
 
-export function EditMarker({ closeModal, id, varEdit }) {
+export function EditMarker({ closeModal, id }) {
   const [isChecked, setIsChecked] = useState(false);
   const [nome, setNome] = useState("");
   const [bairro, setBairro] = useState("");
@@ -29,97 +30,51 @@ export function EditMarker({ closeModal, id, varEdit }) {
     setFile2(data.files[1] || null);
     setFile3(data.files[2] || null);
   };
+
   useEffect(() => {
     getDataMarker(id);
   }, [id]);
-
-  console.log(file1);
-
-  // const getDataFiles = async (id) => {
-  //   const response = await app.get(
-  //     `/files/110460ef-9803-433e-b5cd-b6a38d7fb4e0`
-  //   );
-  //   // const response = await app.get(`/files/${id}`);
-  //   const data = response.data;
-  //   console.log(data);
-  // };
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
     setStatus(!isChecked);
   };
 
-  // const handleFileChange = (index, e) => {
-  //   const file = e.target.files[0];
-  //   if (file) {
-  //     const newFiles = [...files];
-  //     newFiles[index] = { ...newFiles[index], arquivo: file };
-  //     setFiles(newFiles);
-  //   }
-  // };
-
   const handleUpdateMarker = async () => {
     try {
       const response = await app.put(`/markers/${id}`, {
-        nome: nome,
-        rua: rua,
-        bairro: bairro,
-        numero: numero,
+        nome,
+        rua,
+        bairro,
+        numero,
         cidade: "São Luís",
-        status: status,
+        status,
       });
       if (response.status === 200) {
-        // handleUpdateFiles();
         handleNewFiles();
       }
-      // document.location.reload(true);
-      // alert("Marcador atualizado!");
     } catch {
       alert("Ocorreu um erro. Tente novamente.");
     }
   };
 
-  // const handleUpdateFiles = async () => {
-  //   // Deletar todos os arquivos existentes
-
-  //   for (const file of initialFiles) {
-  //     if (file && file.id) {
-  //       try {
-  //         const response = await app.delete(`/files/${file.id}`);
-  //         console.log(`Arquivo ${file.id} deletado: `, response);
-  //       } catch (error) {
-  //         console.log("Erro ao apagar arquivo: ", error);
-  //       }
-  //     }
-  //   }
-
-  //   // console.log("FormData para upload: ", formData);
-
-  //   // try {
-  //   //   if (initialFiles[0]) {
-  //   //     const response1 = await app.put(`/files/${initialFiles[0].id}`, file1);
-  //   //     console.log("Response1 ", response1);
-  //   //   }
-  //   //   if (initialFiles[1]) {
-  //   //     const response2 = await app.put(`/files/${initialFiles[1].id}`, file2);
-  //   //     console.log("Response2 ", response2);
-  //   //   }
-  //   //   if (initialFiles[2]) {
-  //   //     const response3 = await app.put(`/files/${initialFiles[2].id}`, file3);
-  //   //     console.log("Response3 ", response3);
-  //   //   }
-  //   //   // const response = await app.post("/files", formData);
-  //   // } catch (error) {
-  //   //   console.log("Erro ao enviar arquivos: ", error);
-  //   // }
-  // };
+  //config do alert
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
 
   const handleNewFiles = async () => {
-    // Adicionar novos arquivos após deletar todos os antigos
     const formData = new FormData();
     formData.append("id_marker", id);
 
-    // Adicionar os novos arquivos se existirem
     if (file1) formData.append("file1", file1);
     if (file2) formData.append("file2", file2);
     if (file3) formData.append("file3", file3);
@@ -127,19 +82,33 @@ export function EditMarker({ closeModal, id, varEdit }) {
     try {
       const response = await app.post("/files", formData);
       console.log(formData);
-      console.log(response.data);
-    } catch {
-      console.log("Erro: ", Error);
+      Toast.fire({
+        icon: "success",
+        title: `${response?.data?.message}`
+      });
+    } catch (error) {
+      console.log("Erro: ", error);
+      Toast.fire({
+        icon: "error",
+        title: "Erro ao carregar os arquivos"
+      });
     }
   };
 
-  const handleRemoveFile = (idFile) => {
+  const handleRemoveFile = async (idFile, setFile) => {
     try {
-      const res = app.delete(`/files/${idFile}`);
-      console.log(res.data);
-      getDataMarker(id);
+      const res = await app.delete(`/files/${idFile}`);
+      setFile(null);
+      Toast.fire({
+        icon: "success",
+        title: "Arquivo deletado com sucesso"
+      });
     } catch (error) {
       console.log("Erro:", error);
+      Toast.fire({
+        icon: "error",
+        title: "Erro ao deletar o arquivo"
+      });
     }
   };
 
@@ -200,90 +169,37 @@ export function EditMarker({ closeModal, id, varEdit }) {
       <div className="flex flex-col w-full space-y-2">
         <p>Uploads</p>
         <div className="flex flex-col md:flex-row w-full h-44 bg-background border border-orange rounded-lg p-2 space-y-4 md:space-y-0 md:space-x-5">
-          <div className="relative flex items-center justify-center w-full bg-background border border-orange rounded-lg p-1">
-            <input
-              type="file"
-              className="absolute inset-0 opacity-0 cursor-pointer"
-              onChange={(e) => setFile1(e.target.files[0])}
-            />
-            {file1 ? (
-              <div className="relative w-full h-full">
-                <img
-                  src={
-                    file1.arquivo ? file1.arquivo : URL.createObjectURL(file1)
-                  }
-                  alt="Preview"
-                  className="object-cover h-full w-full rounded-lg"
-                />
-                <button
-                  onClick={() => handleRemoveFile(file1.id)}
-                  className="absolute top-2 right-2 p-1 bg-red-600 text-white rounded-full focus:outline-none"
-                >
-                  <FaTrashAlt />
-                </button>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center text-center w-full h-full text-gray-500">
-                Clique para carregar um arquivo
-              </div>
-            )}
-          </div>
-          <div className="relative flex items-center justify-center w-full bg-background border border-orange rounded-lg p-1">
-            <input
-              type="file"
-              className="absolute inset-0 opacity-0 cursor-pointer"
-              onChange={(e) => setFile2(e.target.files[0])}
-            />
-            {file2 ? (
-              <div className="relative w-full h-full">
-                <img
-                  src={
-                    file2.arquivo ? file2.arquivo : URL.createObjectURL(file2)
-                  }
-                  alt="Preview"
-                  className="object-cover h-full w-full rounded-lg"
-                />
-                <button
-                  onClick={() => handleRemoveFile(file2.id)}
-                  className="absolute top-2 right-2 p-1 bg-red-600 text-white rounded-full focus:outline-none"
-                >
-                  <FaTrashAlt />
-                </button>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center text-center w-full h-full text-gray-500">
-                Clique para carregar um arquivo
-              </div>
-            )}
-          </div>
-          <div className="relative flex items-center justify-center w-full bg-background border border-orange rounded-lg p-1">
-            <input
-              type="file"
-              className="absolute inset-0 opacity-0 cursor-pointer"
-              onChange={(e) => setFile3(e.target.files[0])}
-            />
-            {file3 ? (
-              <div className="relative w-full h-full">
-                <img
-                  src={
-                    file3.arquivo ? file3.arquivo : URL.createObjectURL(file3)
-                  }
-                  alt="Preview"
-                  className="object-cover h-full w-full rounded-lg"
-                />
-                <button
-                  onClick={() => handleRemoveFile(file3.id)}
-                  className="absolute top-2 right-2 p-1 bg-red-600 text-white rounded-full focus:outline-none"
-                >
-                  <FaTrashAlt />
-                </button>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center text-center w-full h-full text-gray-500">
-                Clique para carregar um arquivo
-              </div>
-            )}
-          </div>
+          {[file1, file2, file3].map((file, index) => (
+            <div key={index} className="relative flex items-center justify-center w-full bg-background border border-orange rounded-lg p-1">
+              <input
+                type="file"
+                className="absolute inset-0 opacity-0 cursor-pointer"
+                onChange={(e) => {
+                  const setFile = [setFile1, setFile2, setFile3][index];
+                  setFile(e.target.files[0]);
+                }}
+              />
+              {file ? (
+                <div className="relative w-full h-full">
+                  <img
+                    src={file.arquivo ? file.arquivo : URL.createObjectURL(file)}
+                    alt="Preview"
+                    className="object-cover h-full w-full rounded-lg"
+                  />
+                  <button
+                    onClick={() => handleRemoveFile(file.id, [setFile1, setFile2, setFile3][index])}
+                    className="absolute top-2 right-2 p-1 bg-red-600 text-white rounded-full focus:outline-none"
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </div>
+              ) : (
+                <div className="flex items-center justify-center text-center w-full h-full text-gray-500">
+                  Clique para carregar um arquivo
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex flex-col md:flex-row items-center justify-end space-x-10">
